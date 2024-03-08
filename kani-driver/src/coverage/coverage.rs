81b18084aff5386f82301a7c1766908808e69381
Adrian Palacios <accorell@amazon.com>
2024-03-08 20:39:08 +0000

    Build project and run harnesses in workflow

--- kani-driver/src/coverage/coverage.rs
+++ kani-driver/src/coverage/coverage.rs
@@ -1,9 +1,21 @@
 use crate::args::coverage_args::CargoCoverageArgs;
+use crate::KaniSession;
+use crate::project;
+use crate::harness_runner;
 use anyhow::Result;
+use tracing::debug;
 
 // Copyright Kani Contributors
 // SPDX-License-Identifier: Apache-2.0 OR MIT
-pub fn coverage_cargo(_args: CargoCoverageArgs) -> Result<()> {
+pub fn coverage_cargo(mut session: KaniSession, args: CargoCoverageArgs) -> Result<()> {
+    session.args.coverage = true;
+    let project = project::cargo_project(&session, false)?;
+    let harnesses = session.determine_targets(&project.get_all_harnesses())?;
+    debug!(n = harnesses.len(), ?harnesses, "coverage_cargo");
+
+    // Verification
+    let runner = harness_runner::HarnessRunner { sess: &session, project: &project };
+    let results = runner.check_all_harnesses(&harnesses)?;
     Ok(())
     //let install = InstallType::new()?;
     //cargo_test(&install, args)
