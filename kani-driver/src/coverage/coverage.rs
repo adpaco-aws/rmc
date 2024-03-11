578bdadbd3965c436140d76e24df2646d5c52828
Adrian Palacios <accorell@amazon.com>
2024-03-11 20:56:58 +0000

    Add `cargo_prof`

--- kani-driver/src/coverage/coverage.rs
+++ kani-driver/src/coverage/coverage.rs
@@ -4,6 +4,12 @@ use crate::project;
 use crate::harness_runner;
 use anyhow::Result;
 use tracing::debug;
+use crate::session;
+use std::process::Command;
+use crate::OsString;
+use crate::call_single_file::base_rustc_flags;
+use crate::session::lib_playback_folder;
+use crate::session::InstallType;
 
 // Copyright Kani Contributors
 // SPDX-License-Identifier: Apache-2.0 OR MIT
@@ -16,7 +22,68 @@ pub fn coverage_cargo(mut session: KaniSession, args: CargoCoverageArgs) -> Resu
     // Verification
     let runner = harness_runner::HarnessRunner { sess: &session, project: &project };
     let results = runner.check_all_harnesses(&harnesses)?;
+
+    // Now we should save the results
+    // And start the regular compilation
+
+    let install = InstallType::new()?;
+    let _ = cargo_prof(&install, args);
     Ok(())
-    //let install = InstallType::new()?;
-    //cargo_test(&install, args)
+}
+
+/// Does `cargo run` with same toolchain and instrument flag to produce profraw file
+fn cargo_prof(install: &InstallType, args: CargoCoverageArgs) -> Result<()> {
+    let mut rustc_args = vec![];//base_rustc_flags(lib_playback_folder()?);
+    let mut cargo_args: Vec<OsString> = vec!["run".into()];
+
+    rustc_args.extend_from_slice(
+        &["-C", "instrument-coverage", "--emit=mir"].map(OsString::from),
+    );
+    // rustc_args.extend_from_slice(
+    //     &[
+    //         "-C",
+    //         "panic=abort",
+    //         "-C",
+    //         "symbol-mangling-version=v0",
+    //         "-Z",
+    //         "panic_abort_tests=yes",
+    //     ]
+    //     .map(OsString::from),
+    // );
+    // rustc_args.push("--kani-compiler".into());
+    // if args.playback.common_opts.verbose() {
+    //     cargo_args.push("-vv".into());
+    // } else if args.playback.common_opts.quiet {
+    //     cargo_args.push("--quiet".into())
+    // }
+
+    // if args.playback.message_format == MessageFormat::Json {
+    //     cargo_args.push("--message-format=json".into());
+    // }
+
+    // if args.playback.only_codegen {
+    //     cargo_args.push("--no-run".into());
+    // }
+
+    // cargo_args.append(&mut args.cargo.to_cargo_args());
+    // cargo_args.append(&mut cargo_config_args());
+
+    // // These have to be the last arguments to cargo test.
+    // if !args.playback.test_args.is_empty() {
+    //     cargo_args.push("--".into());
+    //     cargo_args.extend(args.playback.test_args.iter().map(|arg| arg.into()));
+    // }`§`
+
+    // Arguments that will only be passed to the target package.
+    let mut cmd = Command::new("cargo");
+    cmd.arg(session::toolchain_shorthand())
+        .args(&cargo_args)
+        // .env("RUSTC", &install.kani_compiler()?)
+        // Use CARGO_ENCODED_RUSTFLAGS instead of RUSTFLAGS is preferred. See
+        // https://doc.rust-lang.org/cargo/reference/environment-variables.html
+        .env("CARGO_ENCODED_RUSTFLAGS", rustc_args.join(&OsString::from("\x1f")));
+        // .env("CARGO_TERM_PROGRESS_WHEN", "never");
+
+    session::run_terminal(&args.coverage.common_opts, cmd)?;
+    Ok(())
 }
